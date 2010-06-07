b5f5b599b6adbfadba1c832744a3e74fab540106
epaull <epaull@soe.ucsc.edu>
2010-06-07 14:27:11 -0700

    Add jqueryui tabs widget

---
+++ web_static/js/beast.js
@@ -0,0 +1,3 @@
+<link type="text/css" href="css/themename/jquery-ui-1.8.custom.css" rel="Stylesheet" />	
+<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="js/jquery-ui-1.8.custom.min.js"></script>
