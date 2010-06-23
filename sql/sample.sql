65efd6116c91c8580dcfd3cd6f26132e4a9c727a
samb <samb@soe.ucsc.edu>
2010-06-23 10:42:10 -0700

    sample sql

--- sql/sample.sql
+++ sql/sample.sql
@@ -1,2 +1,5 @@
- 
-y
+SELECT * FROM entity e, entity_set_entity ese, entity_set es WHERE e.id=ese.entity_id AND es.id=ese.entity_set_id;
+SELECT * FROM entity e, entity_set_entity ese, entity_set es WHERE e.id=ese.entity_id AND es.id=ese.entity_set_id AND es.name='BMW';
+SELECT * FROM entity e, entity_set_entity ese, entity_set es WHERE e.id=ese.entity_id AND es.id=ese.entity_set_id AND es.name in ('Honda', 'Toyota');
+SELECT * FROM entity e, entity_set_entity ese, entity_set es WHERE e.id=ese.entity_id AND es.id=ese.entity_set_id AND es.name='SUV';
+
