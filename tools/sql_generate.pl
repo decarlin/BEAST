9da4b9822c7d7ba7a0239fe99c7e0533b7e7850f
epaull <epaull@soe.ucsc.edu>
2010-06-28 11:18:34 -0700

    support NULL values

--- tools/sql_generate.pl
+++ tools/sql_generate.pl
@@ -13,6 +13,7 @@ my $result = GetOptions("type=s" => \$type);
 
 our $tables = {
 	'meta' 		=> "INSERT INTO meta ('id', 'name') VALUES ('var1', 'var2');",
+	# sets_meta_id = parent, sets_id = child if set, meta_meta_id = child if meta
 	'meta_sets' 	=> "INSERT INTO meta_sets ('sets_meta_id', 'sets_id', 'meta_meta_id') VALUES ('var1', 'var2', 'var3');",
 	'entity' 	=> "INSERT INTO entity ('id', 'name','description' , 'entity_key', 'keyspace_id') VALUES ('var1', 'var2', 'var3', 'var4', 'var5');",
 	'set_entity' 	=> "INSERT INTO set_entity ('sets_id', 'entity_id', 'member_value') VALUES ('var1', 'var2', 'var3');",
@@ -32,7 +33,11 @@ sub buildSQL
 		my $value = $_;
 		chomp($value);
 		my $substr = "var".$i;
-	
+
+		if ($value eq "NULL") {
+			$value = "";
+		}
+
 		$template =~ s/$substr/$value/;	
 		$i++;
 	}
