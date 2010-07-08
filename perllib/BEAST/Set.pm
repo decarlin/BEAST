30364f4ecb1a9983d129d02e70c970bc7b6ea738
epaull <epaull@soe.ucsc.edu>
2010-07-08 14:13:10 -0700

    -- Implement serialization and de-serialization with the JSON package this has the dual use of providing the heavy lifting to serialize Set objects for Session storage, and providing them in a JSON format to send to other web services (such as the interaction browser) -- Overload the new method so we can create a new set object from a JSON sting

--- perllib/BEAST/Set.pm
+++ perllib/BEAST/Set.pm
@@ -6,6 +6,8 @@ use strict;
 use warnings;
 use lib "/projects/sysbio/map/Projects/BEAST/perllib";
 use DBI;
+# JSON object serialization
+use JSON -convert_blessed_universally;
 
 our $TRUE = 1;
 our $FALSE = 0;
@@ -20,50 +22,49 @@ sub new
 {
 	my $class = shift;
 	# hash ref
-	my $name = shift;
-	my $active = shift;
-	my $metadata = shift;
-	my $elements = shift;
 
-	my $self = {
-		'_name' 	=> $name,
-		# boolean 1=yes, 0=no 
-		'_active'	=> $active,
-	 	'_metadata' 	=> $metadata,
-		#'_metadata' 	=> { 'key2 => 'value1',
-		# 		     'key2 => 'value2
-		'_elements'	=> $elements,
-		# '_elements'	=> { 'name' 	=>  $setObj1,
-		#		     'name2' 	=>  $setObj2
-		'_delim'	=> '^',
-	};
+	my $self;
+	if (@_ >= 2) {
+		my $name = shift;
+		my $active = shift;
+		my $metadata = shift;
+		my $elements = shift;
+
+		$self = {
+			'_name' 	=> $name,
+			# boolean 1=yes, 0=no 
+			'_active'	=> $active,
+	 		'_metadata' 	=> $metadata,
+			#'_metadata' 	=> { 'key2 => 'value1',
+			# 		     'key2 => 'value2
+			'_elements'	=> $elements,
+			# '_elements'	=> { 'name' 	=>  $setObj1,
+			#		     'name2' 	=>  $setObj2
+			'_delim'	=> '^',
+		};
+	} elsif (@_ == 1) {
+		my $json_text = shift;
+
+		my $json = JSON->new->utf8;
+		$self = \$json->decode($json_text);
+	} elsif (!@_) {
+		die "Set::new method called without arguments!";
+	}
 
 	bless $self, $class;
 	return $self;
 }
 
+# this converts it to a json string
 sub serialize
 {
 	my $self = shift;
 	
-	my $str = $self->{'_name'}.$self->{'_delim'};
-	foreach (keys %{$self->{'_metadata'}}) {
-		$str = $str.$_."=".$self->{'_metadata'}->{$_}.$self->{'_delim'};
-	}
-	foreach (keys %{$self->{'_elements'}}) {
-	
-		my $name = $_;
-		my $element = $self->get_element($_);
-		if (ref($element) eq 'Set') {
-			$str = $str."\t"."[ ";
-			$str = $str.$element->serialize();
-			$str = $str." ]";
-		} else {
-			$str = $str."\t".$name;
-		}
-	}
+	my $json = JSON->new->utf8;
+	$json = $json->convert_blessed([1]);
+	my $json_text = $json->encode($self);
 
-	return $str;
+	return $json_text;
 }
 
 sub get_name
@@ -103,85 +104,6 @@ sub get_metadata_value
 	my $key = shift;
 
 	return $self->{'_metadata'}->{$key};
-}
-
-#
-# Parse Lines, return set objects
-#
-sub parseSetLines
-{
-	my @lines = @_;
-
-	my @sets;
-
-
-	foreach (@lines) 
-	{
-		my $line = $_;
-		chomp($line);
-		next unless ($line =~ /\S+\s+/);
-		$line =~ /(\S+)\^\t(.*)/;
-		my @components = split (/\^/,$1);
-		my $subsets = $2;
-
-		## create a set object
-		my $name = $components[0];
-		my $metadata = {};
-		my $elements = {};
-		my $i = 0;
-
-		for (@components) 
-		{
-			# the first element is the name
-			if ($i == 0) { $i++; next; }
-
-			my $component = $_;
-			# metadata goes in with key/value pairs
-			if ($component =~ /(.*)=(.*)/) {
-				$metadata->{$1} = $2;
-			} 
-		}
-
-
-		# tab delineated elements
-		my $parse_state = 0;
-		my $parse_string = "";
-		foreach (split(/\t/, $subsets)) {
-			my $part = $_;
-			if ($part =~ /\[/) {
-				if ($parse_state == 0) {
-					$parse_string = $part;
-				} else {	
-					$parse_string = $parse_string."\t".$part;
-				}
-				$parse_state++;
-				next;
-			} elsif ($part =~ /\]/) {
-				$parse_state--;
-			        $parse_string = $parse_string."\t".$part;
-				if ($parse_state == 0) {
-				  $parse_string =~ /^\[ (.*) \]$/;
-			  	  my @ln = ($1);
-			  	  my @setS = parseSetLines(@ln); 
-				  my $setname = $setS[0]->get_name;
-			  	  $elements->{$setname} = $setS[0];	
-				  $parse_string = "";
-				}
-				next;
-			} elsif ($parse_state > 0) {
-			        $parse_string = $parse_string."\t".$part;
-				next;
-			}
-
-			# else 
-		   	$elements->{$part} = 1;	
-		}
-
-		my $set = Set->new($name, 1, $metadata, $elements);
-		push @sets, $set;
-	}
-
-	return @sets;
 }
 
 #
