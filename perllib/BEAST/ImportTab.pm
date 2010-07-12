55a00bf0d8dfed0342beb67b48b83afa1be46f9a
samb <samb@soe.ucsc.edu>
2010-07-12 14:39:32 -0700

    Change multiline string delimiter from "EOF" to "MULTI_LINE_STR"

--- perllib/BEAST/ImportTab.pm
+++ perllib/BEAST/ImportTab.pm
@@ -19,7 +19,8 @@ use BEAST::Set;
 sub new
 {
 	my $class = shift;
-	my $self = {
+	my $self = 
+	{
 		_input 		=> shift,
 	};
 
@@ -35,34 +36,38 @@ sub printImportTab
 	# Hash reference: keys are refs to arrays of strings
 	my $input = $self->{'_input'};
 
-	my $metadata = {
+	my $metadata = 
+	{
 		'db_origin' => [ 'kegg', 'wikipathways', 'reactone' ],
 		'genespace' => [ 'entrez' ],
 	};
 
 	my $importtext = "";
 	my @sets;
-	if ($input->param('importtype') eq 'text') {
-		if ($input->param('importtext')) {
+	if ($input->param('importtype') eq 'text') 
+	{
+		if ($input->param('importtext')) 
+		{
 			$importtext = $input->param('importtext');
 			my @lines = split(/\n/, $importtext);
 			@sets = Set::parseSetLines($metadata, @lines);	
 		}
-	} else {
+	}
+	else 
+	{
 		#my $uploaded_filehandle = $input->upload('importtext');
 	}
 
-	print <<EOF;
+	print <<MULTI_LINE_STR;
 	<form id="importform">
-        <p class='radiO_selectors' id='textStyle'> 
-	<input type='radio' name='importType' checked='checked' value='text' onclick='chooseTextImport(this.form)'>
-	Enter sets to import
-	<br>
-EOF
+		<p class='radiO_selectors' id='textStyle'> 
+			<input type='radio' name='importType' checked='checked' value='text' onclick='chooseTextImport(this.form)'/> Enter sets to import<br/>
+MULTI_LINE_STR
 
 	my $formMetadata = {};
 	my @formMetadata = $input->param('metadata[]');
-	foreach (@formMetadata) {
+	foreach (@formMetadata)
+	{
 		my ($key, $value) = split(/:/, $_);
 		$formMetadata->{$key} = $value;
 	}
@@ -72,37 +77,40 @@ EOF
 		my $type = $_;
 		my $key = "metadata_".$type;
 		print "<b>$type&nbsp&nbsp</b><select name='$key'>";
-		foreach (@{$metadata->{$type}}) {
-			if ($formMetadata->{$key} eq $_) {
+		foreach (@{$metadata->{$type}})
+		{
+			if ($formMetadata->{$key} eq $_)
+			{
 				print "<option value='$_' selected>$_</option>";
-			} else {
+			}
+			else
+			{
 				print "<option value='$_'>$_</option>";
 			}
 		}
 		print "</select><br>";
 	}
 
-	print <<EOF;
-	</p>	
-	<textarea name="importtext" id="setsImportFromText" cols="40" rows="5">$importtext</textarea><br>
-	<p>
-        <p class='radiO_selectors' id='fileStyle'> 
-	<input type='radio' name='importType' value='file' onclick='chooseFileImport(this.form)'>
-	Or import from a local file:
-	</p>
-	<input type='hidden' name='MAX_FILE_SIZE" value='200'>
-	<input type='file' name="importtext" accept="text" id="setsImportFromFile" value="file" onclick="selectImportFile(this.form)">
-	<input type='button' value='Submit' onClick="return onImportSets(this.form);"><br>
+	print <<MULTI_LINE_STR;
+		</p>
+		<textarea name="importtext" id="setsImportFromText" cols="40" rows="5">$importtext</textarea><br/>
+			<p class='radiO_selectors' id='fileStyle'> 
+				<input type='radio' name='importType' value='file' onclick='chooseFileImport(this.form)'>
+				Or import from a local file:
+			</p>
+			<input type='hidden' name='MAX_FILE_SIZE" value='200'/>
+			<input type='file' name="importtext" accept="text" id="setsImportFromFile" value="file" onclick="selectImportFile(this.form)"/>
+			<input type='button' value='Upload' onClick="return onImportSets(this.form);"/><br/>
 	</form>
 	<p>Sets:</p>
 	<p>
-EOF
+MULTI_LINE_STR
 	MySets::displaySets("import", @sets);
 	print "</p>";
 	
 
 	## send back the sets here
-	return @sets;	
+	return @sets;
 }
 
 1;
