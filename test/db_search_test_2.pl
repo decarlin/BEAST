da2b206ff7a7ada506c17324a8b4637e63a39e14
epaull <epaull@soe.ucsc.edu>
2010-07-21 14:38:09 -0700

    - targeted a specific UI bug with this test

---
+++ test/db_search_test_2.pl
@@ -0,0 +1,34 @@
+#!/usr/local/bin/perl
+
+use strict;
+use warnings;
+
+use lib "/projects/sysbio/map/Projects/BEAST/perllib";
+
+use BEAST::BeastDB;
+use BEAST::Search;
+use BEAST::MySets;
+use Data::Dumper;
+
+our $beastDB = BeastDB->new;
+$beastDB->connectDB();
+
+my $treeBuilder = Search->new($beastDB);
+
+
+my @merged = $treeBuilder->searchOnSetDescriptions('reproduction');
+
+my $json_text;
+$json_text = $merged[1]->serialize();
+#print Data::Dumper->Dump([$top_level_nodes[0]]);
+print "encoded:\n";
+print "$json_text:\n";
+my $newset = Set->new($json_text);
+#print Data::Dumper->Dump([$newset]);
+my $new_text =  $newset->serialize();
+print "\n";
+print $new_text;
+
+
+$beastDB->disconnectDB();
+
