d44193b2508cab5c5094ea65fe9aae23893ed1f4
epaull <epaull@soe.ucsc.edu>
2010-08-12 15:43:54 -0700

    -- getting there

---
+++ tools/add_chemdiv/extract_chem_gene_data.pl
@@ -0,0 +1,61 @@
+#!/usr/local/bin/perl
+
+
+use lib "/projects/sysbio/map/Projects/BEAST/perllib";
+use strict;
+
+sub usage 
+{
+	print <<EOF;
+##
+## Usage:  perl $0 --chemdata=raw_file 
+##
+EOF
+}
+
+use Getopt::Long;
+use Math::BigFloat;
+use BEAST::BeastDB;
+
+my $chemdata_file = '';
+GetOptions("chemdata_file=s" => \$chemdata_file);
+
+die &usage() unless (-f $chemdata_file);
+
+# 1 -> ...
+my $columns = []; 
+my $lineno = 1;
+open (CHD, $chemdata_file) || die;
+while (my $line = <CHD>) {
+	chomp($line);
+
+	if ($lineno == 1) {
+		my @column_names = split(/\t/,$line);
+		# line 1 is the ORF
+		for my $i (1 .. (scalar(@column_names) - 1)) {
+			$columns->[$i] = { 
+				'name' => $column_names[$i],
+				'genes' => [],
+			};	
+		}
+		$lineno++;
+		next;
+	}
+
+	my @components = split(/\t/, $line);
+	my $yeast_gene = $components[0];
+	for my $i (1 .. (scalar(@$columns) - 1)) {
+			push @{$columns->[$i]->{'genes'}}, { 'gene' => $yeast_gene, 'value' => $components[$i] };
+	}	
+
+	$lineno++;
+}	
+close (CHD);
+
+#my $hash = $columns->[2];
+#print "for chemical:".$hash->{'name'}."\n";
+#foreach ( @{$hash->{'genes'}}) {
+#	my $hash = $_;
+#	print $hash->{'gene'}.":".$hash->{'value'};
+#	print "\n";
+#}
