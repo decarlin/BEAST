8bb023788758243cb082a032b476a1ac1ed41300
epaull <epaull@soe.ucsc.edu>
2010-08-17 16:20:51 -0700

    -- implement color shading/gradient based on membership strength

--- javalib/src/heatmap/src/Cell.java
+++ javalib/src/heatmap/src/Cell.java
@@ -38,21 +38,38 @@ public class Cell {
     public void setCoordinates(double xstart, double ystart, double width, double height) {
         this.rect = new Rectangle2D.Double(xstart, ystart, width, height);
     }
+
+    public static Color shade(Color color, double fraction) {
+        int red   = (int) Math.round (color.getRed() * fraction);
+        int green = (int) Math.round (color.getGreen() * fraction);
+        int blue  = (int) Math.round (color.getBlue()  * fraction);
+
+        if (red   < 0) red   = 0; else if (red   > 255) red   = 255;
+        if (green < 0) green = 0; else if (green > 255) green = 255;
+        if (blue  < 0) blue  = 0; else if (blue  > 255) blue  = 255;    
+
+        int alpha = color.getAlpha();
+
+        return new Color (red, green, blue, alpha);
+    }
     
     public void drawCellToCanvas(Graphics2D g2, Color memberColor, Color neutral, Color antiMemberColor) {
         
         // fade to the level of membership value
+        Color cellColor;
         if (getMembershipValue() > 0) {
-            g2.setColor(memberColor);
+            cellColor = memberColor;
         } else if (getMembershipValue() == 0) {
-            g2.setColor(neutral);
+            cellColor = neutral;
         } else {
-            g2.setColor(antiMemberColor);
+            cellColor = antiMemberColor;
         }
-    
+        
+        cellColor = shade(cellColor, Math.abs(getMembershipValue()));
+        
+        g2.setColor(cellColor);   
         g2.fill(rect);
         //g2.setColor(Color.GRAY);
         g2.draw(this.rect);
     }
-
 }
